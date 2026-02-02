0383936ff89d26f8a3b99031989184ca31c6c32f
scavenblaze <yashghogare123@gmail.com>
2026-02-03 00:18:54 +0530

    Update Image Upload Endpoint

--- Backend/main.py
+++ Backend/main.py
@@ -1,21 +1,14 @@
-import uuid
-from datetime import datetime
-from fastapi import FastAPI, UploadFile, File, HTTPException
 import os
 from dotenv import load_dotenv
+from fastapi import FastAPI, UploadFile, File, HTTPException
 from supabase import create_client, Client
+import uuid
 
 load_dotenv()
 
-def get_env(name: str) -> str:
-    value = os.getenv(name)
-    if not value:
-        raise RuntimeError(f"Missing required environment variable: {name}")
-    return value
-
-SUPABASE_URL: str = get_env("SUPABASE_URL")
-SUPABASE_SERVICE_KEY: str = get_env("SUPABASE_SERVICE_KEY")
-SUPABASE_BUCKET: str = os.getenv("SUPABASE_BUCKET", "upload_images")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_SERVICE_KEY = os.getenv("SUPABASE_SERVICE_KEY")
+SUPABASE_BUCKET = os.getenv("SUPABASE_BUCKET", "upload_images")
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_SERVICE_KEY)
 
@@ -24,29 +17,28 @@ app = FastAPI()
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
     try:
+        #Create unique file id for the uploaded image
         file_id = str(uuid.uuid4())
         ext = file.filename.split(".")[-1]
         storage_path = f"{file_id}.{ext}"
 
         file_bytes = await file.read()
 
-        # Upload file (raises error automatically if it fails)
+        #Upload file to storage
         supabase.storage.from_(SUPABASE_BUCKET).upload(
             path=storage_path,
             file=file_bytes,
             file_options={"content-type": file.content_type},
         )
 
-        # Get public URL
+        #Generate file url
         public_url = supabase.storage.from_(SUPABASE_BUCKET).get_public_url(storage_path)
         if isinstance(public_url, dict):
             public_url = public_url.get("publicUrl")
 
-        # Insert metadata
+        #Insert to db
         db_response = supabase.table("Image").insert({
-            "id": file_id,
             "image_path": public_url,
-            "uploaded_at": datetime.utcnow().isoformat()
         }).execute()
 
         return db_response.data[0]

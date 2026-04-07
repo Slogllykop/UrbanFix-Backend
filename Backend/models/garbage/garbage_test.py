076e2d43270c152d48a611aef6bd09721c0f284f
scavenblaze <yashghogare123@gmail.com>
2026-04-07 23:29:17 +0530

    fix: remove old code

--- Backend/models/garbage/garbage_test.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-# Load ONNX model
-model = YOLO("best.onnx")
-
-# Run prediction on an image
-results = model.predict(source="image_no.jpg", save=True, conf=0.25)
-
-print(results)
--- Backend/models/pothole/model_test.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-# Load ONNX model
-model = YOLO("best.onnx")
-
-# Run prediction on an image
-results = model.predict(source="multi.jpg", save=True, conf=0.25)
-
-print(results)
